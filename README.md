d92fe5ca38578b4b9250f3f0167cb95c7577f916
Johnny Reina <johnny.r.reina@gmail.com>
2020-01-15 14:05:25 -0600

    grade-school complete

---
+++ README.md
@@ -0,0 +1,5 @@
+# Exercism.io Haskell Track
+These are my exercises from the Exercism.io mentored Haskell track.
+
+## Why?
+I am terrible with Haskell and I hope to get better. 
---
+++ haskell/grade-school/src/School.hs
@@ -0,0 +1,22 @@
+module School (School, add, empty, grade, sorted) where
+
+import Data.List (sort, sortBy)
+
+data School = School { students :: [(Int, String)] }
+
+-- I did not write this. I found this on StackOverflow
+unique :: Eq a => [a] -> [a]
+unique [] = []
+unique (x:xs) = x:unique (filter ((/=) x) xs)
+
+add :: Int -> String -> School -> School
+add gradeNum student school = School $ (gradeNum, student):(students school)
+
+empty :: School
+empty = School []
+
+grade :: Int -> School -> [String]
+grade gradeNum school = sort $ snd <$> filter ((== gradeNum) . fst) (students school)
+
+sorted :: School -> [(Int, [String])]
+sorted school = sortBy (\(a,_) (b,_) -> compare a b) $ fmap (\grd -> (grd, sort $ fmap snd $ filter ((== grd) . fst) (students school))) (unique $ fst <$> students school)
