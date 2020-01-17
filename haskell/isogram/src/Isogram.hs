aa29e3ff582432d9b524a39f73ab09342d5bc226
Johnny Reina <johnny.r.reina@gmail.com>
2020-01-17 01:13:12 -0600

    Adds completed isogram assignment

---
+++ haskell/isogram/src/Isogram.hs
@@ -0,0 +1,15 @@
+module Isogram
+  ( isIsogram
+  ) where
+
+import Data.Char (toLower)
+
+isIsogramR :: String -> Bool
+isIsogramR "" = True
+isIsogramR (x:xs) =
+  (x `elem` "- " || not (toLower(x) `elem` xs)) &&
+  isIsogram (xs)
+
+isIsogram :: String -> Bool
+isIsogram "" = True
+isIsogram xs = isIsogramR (toLower <$> xs)
